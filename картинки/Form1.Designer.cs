30c42be08fb60d3bb120ceea6d880cb6b5c72fed
TonyGoose <preparefight@yandex.ru>
2023-06-24 23:23:15 +0300

    Добавьте файлы проекта.

---
+++ картинки/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace картинки
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.открыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(78, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(710, 366);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открыToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // открыToolStripMenuItem
+            // 
+            this.открыToolStripMenuItem.Name = "открыToolStripMenuItem";
+            this.открыToolStripMenuItem.Size = new System.Drawing.Size(12, 20);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.button3.Location = new System.Drawing.Point(0, 139);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 67);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Выбор рисунка";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(78, 384);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(79, 41);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "В серый цвет";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(536, 385);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 40);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Вернуть оригинал";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Red;
+            this.button6.Location = new System.Drawing.Point(182, 385);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(79, 40);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "В красный цвет";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button7.Location = new System.Drawing.Point(278, 385);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 40);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "В синий цвет";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Lime;
+            this.button8.Location = new System.Drawing.Point(389, 384);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 41);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "В зеленый цвет";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.button2.Location = new System.Drawing.Point(662, 384);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 41);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Сохранить изображение";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Конвертор рисунков";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem открыToolStripMenuItem;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ картинки/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace картинки
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Bitmap originalImage;
+        Bitmap currentImage;
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // показать диалоговое окно для выбора файла
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files (*.bmp, *.jpg, *.png)|*.bmp;*.jpg;*.png";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // загрузить выбранный файл как изображение
+                originalImage = new Bitmap(openFileDialog.FileName);
+                currentImage = originalImage;
+
+                // отобразить исходное изображение в PictureBox
+                pictureBox1.Image = originalImage;
+            }
+        }
+
+
+
+
+        private Bitmap ConvertToGray(Bitmap original)
+        {
+            int width = original.Width;
+            int height = original.Height;
+
+            Bitmap grayImage = new Bitmap(width, height, PixelFormat.Format24bppRgb);
+            Graphics graphics = Graphics.FromImage(grayImage);
+
+            ColorMatrix colorMatrix = new ColorMatrix(
+                new float[][]
+                {
+            new float[] {0.299f, 0.299f, 0.299f, 0, 0},
+            new float[] {0.587f, 0.587f, 0.587f, 0, 0},
+            new float[] {0.114f, 0.114f, 0.114f, 0, 0},
+            new float[] {0, 0, 0, 1, 0},
+            new float[] {0, 0, 0, 0, 1}
+                });
+
+            using (ImageAttributes attributes = new ImageAttributes())
+            {
+                attributes.SetColorMatrix(colorMatrix);
+                graphics.DrawImage(original, new Rectangle(0, 0, width, height),
+                    0, 0, width, height, GraphicsUnit.Pixel, attributes);
+            }
+
+            graphics.Dispose();
+
+            return grayImage;
+        }
+
+
+
+
+        public Bitmap ConvertToRed(Bitmap original)
+        {
+            // создать новое изображение той же ширины и высоты
+            Bitmap newImage = new Bitmap(original.Width, original.Height);
+
+            // проход по каждому пикселю исходного изображения
+            for (int i = 0; i < original.Width; i++)
+            {
+                for (int j = 0; j < original.Height; j++)
+                {
+                    Color originalColor = original.GetPixel(i, j);
+
+                    // вычислить оттенок красного
+                    int redValue = originalColor.R;
+                    Color newColor = Color.FromArgb(redValue, 0, 0);
+
+                    // установка нового цвета пикселя в новом изображении
+                    newImage.SetPixel(i, j, newColor);
+                }
+            }
+
+            return newImage;
+        }
+
+        public Bitmap ConvertToBlue(Bitmap original)
+        {
+            // создать новое изображение той же ширины и высоты
+            Bitmap newImage = new Bitmap(original.Width, original.Height);
+
+            // проход по каждому пикселю исходного изображения
+            for (int i = 0; i < original.Width; i++)
+            {
+                for (int j = 0; j < original.Height; j++)
+                {
+                    Color originalColor = original.GetPixel(i, j);
+
+                    // вычислить оттенок синего
+                    int blueValue = originalColor.B;
+                    Color newColor = Color.FromArgb(0, 0, blueValue);
+
+                    // установка нового цвета пикселя в новом изображении
+                    newImage.SetPixel(i, j, newColor);
+                }
+            }
+
+            return newImage;
+        }
+
+
+        public Bitmap ConvertToGreen(Bitmap original)
+        {
+            // создать новое изображение той же ширины и высоты
+            Bitmap newImage = new Bitmap(original.Width, original.Height);
+
+            // проход по каждому пикселю исходного изображения
+            for (int i = 0; i < original.Width; i++)
+            {
+                for (int j = 0; j < original.Height; j++)
+                {
+                    Color originalColor = original.GetPixel(i, j);
+
+                    // вычислить оттенок зеленого
+                    int greenValue = originalColor.G;
+                    Color newColor = Color.FromArgb(0, greenValue, 0);
+
+                    // установка нового цвета пикселя в новом изображении
+                    newImage.SetPixel(i, j, newColor);
+                }
+            }
+
+            return newImage;
+        }
+
+
+
+
+
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (currentImage != null)
+            {
+                // преобразовать изображение в оттенки серого
+                Bitmap grayImage = ConvertToGray(currentImage);
+                currentImage = grayImage;
+
+                // отобразить новое изображение в PictureBox
+                pictureBox1.Image = grayImage;
+            }
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (originalImage != null)
+            {
+                currentImage = originalImage;
+                pictureBox1.Image = originalImage;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (currentImage != null)
+            {
+                // преобразовать изображение в оттенок красного
+                Bitmap redImage = ConvertToRed(currentImage);
+                currentImage = redImage;
+
+                // отобразить новое изображение в PictureBox
+                pictureBox1.Image = redImage;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (currentImage != null)
+            {
+                // преобразовать изображение в оттенок синего
+                Bitmap blueImage = ConvertToBlue(currentImage);
+                currentImage = blueImage;
+
+                // отобразить новое изображение в PictureBox
+                pictureBox1.Image = blueImage;
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            {
+                if (currentImage != null)
+                {
+                    // преобразовать изображение в оттенок зеленого
+                    Bitmap greenImage = ConvertToGreen(currentImage);
+                    currentImage = greenImage;
+
+                    // отобразить новое изображение в PictureBox
+                    pictureBox1.Image = greenImage;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            if (currentImage != null)
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Image Files(*.BMP;*.JPG;*.PNG;*.GIF)|*.BMP;*.JPG;*.PNG;*.GIF";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // сохраняем изображение в том же формате файла, в котором оно было открыто
+                    currentImage.Save(saveFileDialog.FileName, currentImage.RawFormat);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
